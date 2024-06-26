23d95abc3136a5d67050c8f06fe679ce1658f4ac
LingQi <bmlingqi@163.com>
2024-06-26 20:07:36 +0800

    项目初始化

---
+++ src/main/java/com/zeeyeh/mctoosimple/GlobalConfig.java
@@ -0,0 +1,11 @@
+package com.zeeyeh.mctoosimple;
+
+/**
+ * @author Leon_Keiran
+ * @description 全局配置
+ * @date 2024/6/26 下午8:02
+ * @github <a href="https://github.com/bmlingqi">https://github.com/bmlingqi</a>
+ */
+public class GlobalConfig {
+    public static final String MOD_ID = "mctoosimple";
+}
---
+++ src/main/java/com/zeeyeh/mctoosimple/Mctoosimple.java
@@ -0,0 +1,72 @@
+package com.zeeyeh.mctoosimple;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+/**
+ * 模组入口
+ * @author Leon_Keiran
+ * @date 2024/6/26 下午8:03
+ * @github <a href="https://github.com/bmlingqi">https://github.com/bmlingqi</a>
+ */
+@Mod(GlobalConfig.MOD_ID)
+public class Mctoosimple
+{
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Mctoosimple()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        InterModComms.sendTo("mctoosimple", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m->m.messageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
+        {
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+}
